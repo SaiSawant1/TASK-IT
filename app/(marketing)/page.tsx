e87eddeffa3d364f8aa5f69b6d6ebcf0d454aa36
SaiSawant1 <saisawant2003@gmail.com>
2024-10-08 14:44:08 +0530

    build test-2

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -45,7 +45,9 @@ export default function MarketingPage() {
         all by TASK-IT
       </div>
       <Button className="mt-6" size={"lg"} asChild>
-        <Link href={"/sign-up"}>Lets TASK-IT {process.env.NEXTAUTH_URL}</Link>
+        <Link href={"/sign-up"}>
+          Lets TASK-IT {process.env.GOOGLE_CLIENT_SECRET}
+        </Link>
       </Button>
     </div>
   );
