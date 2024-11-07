9fd83f8bd5bad1eaccf51c2ed37dd545bcbe9ced
SaiSawant1 <saisawant2003@gmail.com>
2024-11-08 02:59:34 +0530

    stripe webhook fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,13 +1,12 @@
 import Stripe from "stripe";
 
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import { stripe } from "@/lib/stripe";
 
 export async function POST(req: Request) {
-  const body = await req.text();
-  const signature = headers().get("Stripe-Signature") as string;
+  const body = await (await req.blob()).text();
+  const signature = req.headers.get("stripe-signature") as string;
   let event: Stripe.Event;
 
   try {
